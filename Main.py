d28006456b9da01a519b1849328ee4419508dc43
Marcos <166460088+M4rk0Zoof@users.noreply.github.com>
2024-09-27 02:08:35 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -9,6 +9,32 @@ ven.configure(bg="pale green")
 ven.geometry("700x500")
 
 def main():
+    def infor():
+        server_ip = entrada1.get()
+        server_port = entrada2.get()
+        server_port = int(server_port)
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(10)
+            s.connect((server_ip, server_port))
+            
+            active_connections = len(s.getsockname())
+            print("Número de conexiones activas:",active_connections)
+            
+            process_count = len(psutil.pids())
+            print("Número de procesos en ejecución:",process_count)
+            
+            s.close()
+            
+        except ConnectionRefusedError:
+            print("No se pudo establecer la conexión con el servidor.")
+        except Exception as e:
+            print("Ocurrió un error:",e)
+    
+    def borrado():
+        entrada1.delete(0, 'end')
+        entrada2.delete(0, 'end')
+
     texto1 = Label(ven, text="Ingrese direccion IP: ", bg="yellow")
     texto1.place(relx=0.03, rely=0.03, relwidth=0.18, relheight=0.08)
     entrada1 = Entry(ven, bg="pink")
